634e0a4e49d20f650c4bcab1531474ad0b74bd55
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-10-20 16:07:34 -0400

    update

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -4,3 +4,11 @@ from sqlalchemy import create_engine
 import os
 
 
+load_dotenv()
+
+GCP_MYSQL_HOSTNAME = os.getenv("GCP_MYSQL_HOSTNAME")
+GCP_MYSQL_USER = os.getenv("GCP_MYSQL_USERNAME")
+GCP_MYSQL_PASSWORD = os.getenv("GCP_MYSQL_PASSWORD")
+GCP_MYSQL_DATABASE = os.getenv("GCP_MYSQL_DATABASE")
+
+connection_string_gcp = 
